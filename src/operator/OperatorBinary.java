b26563655e29c76013e4aa0ebb3c350ad781d46c
Rofiah123 <62419412+Rofiah123@users.noreply.github.com>
2020-04-10 20:54:12 +0700

    Operator Binary Assignment

--- src/operator/OperatorBinary.java
+++ src/operator/OperatorBinary.java
@@ -7,8 +7,9 @@ public class OperatorBinary {
     //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
 
     String nama = "Rofi'ah";
-    int jumlah = 24;
+    int jumlahSks = 24;
     double indeksPrestasiKumulatif = 3.89;;
+
     
 
 }
