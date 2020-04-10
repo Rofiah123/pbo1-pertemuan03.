b8403dfa98ef115fa098f8151201f940adddc086
Rofiah123 <62419412+Rofiah123@users.noreply.github.com>
2020-04-10 22:31:18 +0700

    Rofiah jalanjalan

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -2,8 +2,8 @@ package percabangan;
 
 public class IfElse {
     public static void  main(String[] args) {
-        double totalBelanja = 25000;
-        double uangDiDompet = 50000;
+        double totalBelanja = 53000;
+        double uangDiDompet = 25000;
 
         if(uangDiDompet<totalBelanja){
             System.out.println("Uang kurang, kurangi jajan anda");
---
+++ src/percabangan/Switch.java
@@ -0,0 +1,22 @@
+package percabangan;
+
+public class Switch {
+    public static void main(String[] args){
+        int jalanJalan=3;
+        switch(jalanJalan){
+            case 1:
+                System.out.println("Urang mainan");
+            case 2:
+                System.out.println("Urang nongkrong");
+            case 3:
+                System.out.println("Urang pacaran");
+            case 4:
+                System.out.println("Urang Menggosip");
+            case 5:
+                System.out.println("Makan makan");
+            case 6:
+                System.out.println("Urang be tik tok");
+        }
+    }
+
+}
